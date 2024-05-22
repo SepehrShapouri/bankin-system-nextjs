660ab43ae6f3d2b4daf5fc56d2676f21eed9a956
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-05-22 18:07:48 +0330

    feat:folder structure added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
-import { cn } from "@/lib/utils"
-import "@/styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
 
-const fontSans = FontSans({
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  variable: "--font-sans",
-})
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export const metadata: Metadata = {
+  title: "Currencix",
+  description: "All in one transaction managment",
+  icons: "/icons/logo.svg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
         {children}
       </body>
     </html>
-  )
+  );
 }
