30baec270a658fa619ab1c140108b4c4e9ae7014
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-05-22 17:55:42 +0330

    feat:initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils"
+import "@/styles/globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
